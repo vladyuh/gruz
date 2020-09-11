974eed411905b4228cece4aa48961cc3cac53b7e
vladyuh <33131143+vladyuh@users.noreply.github.com>
2020-09-11 13:23:21 +0300

    pagination, 404
    pagination in company orders, added 404 page

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "head.php";
+?>
+
+<link rel="stylesheet" href="/css/index.min.css">
+
+<?php
+    require_once "header.php";
+?>
+
+
+    <main>
+
+        <div class="notFound">
+            <div class="wrapper">
+                <div class="inner">
+                        <div class="title">404</div>
+                        <div class="desc">Страница не найдена</div>
+                        <a href="/" class="flat-btn yellow">На главную</a>
+                </div>
+            </div>            
+        </div>
+
+    </main>
+
+    <?php
+    require_once "footer.php";
+?>
